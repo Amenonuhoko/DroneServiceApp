a3945154d13304e4b10859c556621822c2467854
Mauriza Arianne <76801693+Amenonuhoko@users.noreply.github.com>
2023-04-26 12:19:10 +0800

    new gui

---
+++ DroneServiceApp/Drone.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneServiceApp
+{
+    class Drone
+    {
+        // 6.1	Create a separate class file to hold the data items of the Drone. Use separate getter and setter methods, ensure the attributes are private and the accessor methods are public.
+        // Add a display method that returns a string for Client Name and Service Cost.
+        // Add suitable code to the Client Name and Service Problem accessor methods so the data is formatted as Title case or Sentence case. Save the class as “Drone.cs”.
+
+        private string clientName;
+        private string droneModel;
+        private string serviceProblem;
+        private string serviceCost;
+        private string serviceTag;
+
+        public void displayDrone ()
+        {
+
+        }
+
+        public string getClientName () { return clientName; }
+        public string getDroneModel () { return droneModel; }
+        public string getServiceProblem () { return serviceProblem;}
+        public string getServiceCost () { return serviceCost;}
+        public string getServiceTag () { return serviceTag;}
+
+        public void setClientName (string input) { clientName = input; }
+        public void setDroneModel (string input) { droneModel = input; }
+        public void setServiceProblem (string input) { serviceProblem = input; }
+        public void setServiceCost (string input) { serviceCost = input; }
+        public void setServiceTag (string input) { serviceTag = input; }
+
+
+    }
+}
--- DroneServiceApp/MainWindow.xaml.cs
+++ DroneServiceApp/MainWindow.xaml.cs
@@ -13,6 +13,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+// Mauriza Arianne
+// DroneServiceApp
+// 26/04/2023
+
 namespace DroneServiceApp
 {
     /// <summary>
@@ -23,6 +27,45 @@ namespace DroneServiceApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // 6.2	Create a global List<T> of type Drone called “FinishedList”. 
+        // 6.3	Create a global Queue<T> of type Drone called “RegularService”.
+        // 6.4	Create a global Queue<T> of type Drone called “ExpressService”.
+        List<Drone> FinishedList = new List<Drone>();
+        Queue<Drone> RegularService = new Queue<Drone>();
+        Queue<Drone> ExpressService = new Queue<Drone>();
+
+        // 6.5	Create a button method called “AddNewItem” that will add a new service item to a Queue<> based on the priority.
+        // Use TextBoxes for the Client Name, Drone Model, Service Problem and Service Cost.
+        // Use a numeric up/down control for the Service Tag.
+        // The new service item will be added to the appropriate Queue based on the Priority radio button.
+        public void AddNewItem()
+        {
+            Drone newDrone = new Drone();
+            newDrone.setClientName(txtClientName.Text);
+            newDrone.setDroneModel(txtDroneModel.Text);
+            newDrone.setServiceProblem(txtServiceProblem.Text);
+            newDrone.setServiceTag(txtServiceTag.Text);
+            newDrone.setServiceCost(txtServiceCost.Text);
+            
+
+
+        }
+
+        private void btnServiceTagInc_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(txtServiceTag.Text, out int tag))
+            {
+                if(tag < 900) txtServiceTag.Text = (tag + 10).ToString();
+            }
+        }
+        private void btnServiceTagDec_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(txtServiceTag.Text, out int tag))
+            {
+                if (tag > 100) txtServiceTag.Text = (tag - 10).ToString();
+            }
         }
     }
 }
